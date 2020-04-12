19f302bb5bd0099961bb4f5d3e956a017d739155
AtkSeeGow <atkseegow@gmail.com>
2020-04-12 22:15:07 +0800

    車子移動增加音效、移動調整為鼠標移動距離

--- Assets/Chapter 4/CarController.cs
+++ Assets/Chapter 4/CarController.cs
@@ -1,25 +1,37 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class CarController : MonoBehaviour
 {
     float speed = 0;
 
+    Vector2 startPosition;  // 滑鼠左鍵點擊時的位置
+
+    AudioSource audioSource;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        this.audioSource = this.GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButton(0))
-            this.speed = 0.2f;
+        if (Input.GetMouseButtonDown(0))  // 如果左鍵被按下
+            this.startPosition = Input.mousePosition;  // 將目前滑鼠的位置放到變數中
+
+        if(Input.GetMouseButtonUp(0))
+        {
+            var endPosition = Input.mousePosition;
+
+            float swipeLength = endPosition.x - startPosition.x;
+
+            speed = swipeLength / 500.0f;
+
+            this.audioSource.Play();
+        }
 
         this.transform.Translate(this.speed, 0, 0);
         this.speed *= 0.98f;
-
     }
 }
